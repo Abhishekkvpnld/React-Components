0a04f27f076af21f3ca4c218d9d86c007da2dd7e
Abhishekkv <abhishekkvpnld@gmail.com>
2024-09-05 21:01:34 +0530

    progress bar completed

--- client/src/components/CheckoutStepper.jsx
+++ client/src/components/CheckoutStepper.jsx
@@ -1,74 +1,101 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./checkoutStepper/checkoutStepper.css";
-
 
 const CheckoutStepper = ({ stepsConfig = [] }) => {
 
     const [currentStep, setCurrentStep] = useState(1);
     const [isCompleted, setIsCompleted] = useState(false);
+    const stepRef = useRef([]);
+    const [margin, setMargins] = useState({
+        marginLeft: 0,
+        marginRight: 0
+    });
 
+    useEffect(() => {
+        // console.log(stepRef.current[0].offsetWidth)
+        setMargins({
+            marginLeft: stepRef.current[0].offsetWidth / 2,
+            marginRight: stepRef.current[stepsConfig.length - 1].offsetWidth / 2
+        })
+    }, [stepRef]);
 
     const handleNext = () => {
         setCurrentStep((prev) => {
             if (prev === stepsConfig.length) {
                 setIsCompleted(true);
-                return prev
+                return prev;
             } else {
-                return prev + 1
+                return prev + 1;
             }
-        })
-    }
+        });
+    };
 
-    const ActiveComponent = stepsConfig[currentStep - 1]?.component
+    const calculateProgressBar = () => {
+        return ((currentStep - 1) / (stepsConfig.length - 1)) * 100;
+    };
+
+    const ActiveComponent = stepsConfig[currentStep - 1]?.component;
 
     if (!stepsConfig.length) {
-        return <></>
+        return <></>;
     }
 
     return (
         <>
             <div className="stepper">
-                {
-                    stepsConfig.map((step, index) => {
-                        return (
-                            <div className={`step 
+                {stepsConfig.map((step, index) => (
+                    <div
+                        className={`step 
                                  ${currentStep > index + 1 || isCompleted ? "complete" : ""} 
-                                 ${currentStep === index + 1 ? "active" : ""}`
-                            }
-                                key={step.name}
-                            >
-                                <div
-                                    className={`step-number`}>
-                                    {
-                                        currentStep > index + 1 || isCompleted ? (
-                                            <span>&#10003;</span>
-                                        ) : (
-                                            index + 1
-                                        )
-                                    }
-                                </div>
-                                <div className="step-name">{step.name}</div>
-                            </div>
-                        )
-                    })
-                }
+                                 ${currentStep === index + 1 ? "active" : ""}`}
+                        key={step.name}
+                        ref={(el) => stepRef.current[index] = el}
+                    >
+                        <div className="step-number">
+                            {currentStep > index + 1 || isCompleted ? (
+                                <span>&#10003;</span>
+                            ) : (
+                                index + 1
+                            )}
+                        </div>
+                        <div className="step-name">{step.name}</div>
+                    </div>
+                ))}
+
+                <div className="progress-bar"
+                    style={{
+                        width: `calc(100% - ${margin.marginLeft + margin.marginRight}px)`,
+                        marginLeft: margin.marginLeft,
+                        marginRight: margin.marginRight
+                    }}
+                >
+                    <div className="progress" style={{ width: `${calculateProgressBar()}%` }}></div>
+                </div>
             </div>
 
-            <div style={{display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"column",gap:"10px"}}>
+
+            <div
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                    gap: "10px",
+                }}
+            >
                 <ActiveComponent />
 
-                <button
-                    disabled={isCompleted}
-                    className={`btn ${isCompleted ? "finish" : ""}`}
-                    onClick={handleNext}
-                >
-                    {currentStep === stepsConfig.length ? "Finish" : "Next"}
-                </button>
+                {!isCompleted && (
+                    <button
+                        className={`btn ${currentStep === stepsConfig.length ? "finish" : ""}`}
+                        onClick={handleNext}
+                    >
+                        {currentStep === stepsConfig.length ? "Finish" : "Next"}
+                    </button>
+                )}
             </div>
-
-
         </>
-    )
-}
+    );
+};
 
-export default CheckoutStepper
+export default CheckoutStepper;
