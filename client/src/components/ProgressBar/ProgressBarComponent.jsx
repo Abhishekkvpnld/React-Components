4c82b495aec5511bd963ba8bb9638c0fa662aa57
Abhishekkv <abhishekkvpnld@gmail.com>
2024-09-10 13:29:59 +0530

    progress bar completed

--- client/src/components/ProgressBar/ProgressBarComponent.jsx
+++ client/src/components/ProgressBar/ProgressBarComponent.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
 import "./progressBarComponent.css";
 
 const ProgressBarComponent = ({ value = 0 }) => {
+
+    const [percentage, setPercentage] = useState(0);
+
+    useEffect(() => {
+        setPercentage(Math.min(100, value));
+    }, [value]);
+
     return (
         <div className="progress">
-            <span>{value.toFixed()}%</span>
+            <span style={{ color: `${percentage > 48 ? "white" : "black"}` }}>{percentage.toFixed()}%</span>
+            <div
+                style={{ width: `${percentage}%` }}
+                // style={{
+                //     transform: `scaleX(${percentage / 100})`,
+                //     transformOrigin: "left"
+                // }}
+                role="progressbar"
+                aria-valuemin={0}
+                aria-valuemax={100}
+                aria-valuenow={percentage}
+            />
         </div>
     )
 }
--- client/src/pages/ProgressBar/ProgressBar.jsx
+++ client/src/pages/ProgressBar/ProgressBar.jsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from "react";
 import ProgressBarComponent from "../../components/ProgressBar/ProgressBarComponent";
 import "./progressBar.css";
 
 const ProgressBar = () => {
+
+    const [value, setValue] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setValue((prev) => prev + 1);
+        }, 50);
+
+        if (value === 100) {
+            clearInterval(interval)
+        }
+        return () => clearInterval(interval); // Ensure cleanup when component unmounts
+    }, [value]);
+
     return (
         <div className="progressBar_div">
             <h2>Progress Bar</h2>
-            <ProgressBarComponent/>
+            <ProgressBarComponent value={value} />
             <span>Loading...</span>
         </div>
     )
