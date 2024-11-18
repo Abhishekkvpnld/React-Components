30fd45b453a37a6def005f021fcd09e7aca0d2c8
Abhishekkv <abhishekkvpnld@gmail.com>
2024-11-18 21:31:58 +0530

    new component

--- client/src/components/quizComp/QuizComponent.jsx
+++ client/src/components/quizComp/QuizComponent.jsx
@@ -5,10 +5,10 @@ const QuizComponent = ({ questions, answerClick }) => {
 
     return (
         <div className="questions">
-            <h2>{questions.Question}</h2>
+            <h2>{questions?.Question}</h2>
             <ul className="options">
                 {
-                    questions.answerOptions.map((option) => (
+                    questions.answerOptions?.map((option) => (
                         <li key={option.text} className="list">
                             <button onClick={() => answerClick(option.isCorrect)}>
                                 {option.text}
